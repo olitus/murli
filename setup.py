ffb988a374b269def21dba452dbfaa87bde085ac
Oleksandr Litus <oleks.litus@gmail.com>
2021-02-11 17:52:10 -0500

    added thigs for PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='pyto',
-    version='0.1.0',
-    py_modules=['pyto'],
-    install_requires=[
-        'Click',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mur",
+    version="0.0.1",
+    author="Oleksandr Litus",
+    author_email="oleks.litus@gmail.com",
+    description="Opinionated Python manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olitus/mur",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    entry_points='''
-        [console_scripts]
-        pyto=pyto.main:cli
-    '''
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
 )
